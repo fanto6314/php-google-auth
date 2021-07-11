a682e8ec360eb623c7bf24e618a5cb2814eaeabd
fanto6314 <antonio.foresta04@gmail.com>
2021-07-11 12:03:22 +0200

    [+] First Commit

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
     require_once 'vendor/autoload.php';
     $google_client = new Google_Client();
-    $google_client->setClientId('55263042094-s2v86chg36feh2om3hpt7rpc3k3lf487.apps.googleusercontent.com'); // Add your google client id here
-    $google_client->setClientSecret('uDUhXYMZoOJkDGPynxw9vXFJ'); // Add your google client secret here
-    $google_client->setRedirectUri('http://localhost:3000'); // Add your main page / redirect url here
+    $google_client->setClientId('-'); // Add your google client id here
+    $google_client->setClientSecret('-'); // Add your google client secret here
+    $google_client->setRedirectUri('-'); // Add your main page / redirect url here
     $google_client->addScope('email');
     $google_client->addScope('profile');
     session_start();
