656fe44dc97a53283f5470c7dac99c5d0601289d
Antonio <62208053+fanto6314@users.noreply.github.com>
2021-07-15 22:32:35 +0200

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,6 +1,6 @@
 <?php
     include('config.php');
-    $google_client->revokeToken(); // Deleting google token
+    $google_client->revokeToke(); // Deleting google token
     session_destroy(); // Deleting session data
     header('location:index.php'); // Redirecting to main page
 ?>
